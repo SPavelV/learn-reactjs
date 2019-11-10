5a7e73a83b957085107b3307b441dbbd49ffa578
PavelSolovev <pashius2010@gmail.com>
2019-11-10 12:03:22 +0400

    jsx synax

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './App.css';
+import Car from './Car/Car';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    }
+    return (
+      <div style={divStyle}>
+        <h1>Hello world!</h1>
+        <Car name='Ford' year={2018}>
+          <p style={{color: "orange"}}>Color</p>
+        </Car>
+        <Car name='Audi' year={2016}>
+          <p style={{color: "red"}}>Color</p>
+        </Car>
+        <Car name={'Mazda'} year={2010}/>
+      </div>
+    );
+  }
+ 
+
 }
 
 export default App;
