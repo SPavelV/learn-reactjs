f95e4c3fb25c6d308a34ca232271898453a69da8
Solovev Pavel <p.solovev@elewise.com>
2019-12-08 12:47:04 +0400

    component for active question

--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import classes from './Layout.module.css';
-console.log(classes);
 
 export default class Layout extends Component {
   render() {
     return (
       <div className={classes.Layout}>
-
         <main >
           {this.props.children}
         </main>
