553e8e22af754bbf4461a6ed8b40d9711df8dece
Solovev Pavel <p.solovev@elewise.com>
2019-12-28 16:41:33 +0400

    add button ui component

--- react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
+++ react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./FinishedQuiz.module.css";
+import Button from '../UI/Button/Button';
 
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -33,7 +34,8 @@ const FinishedQuiz = props => {
       </ul> 
       <p>Правильно {successCount} из {props.quiz.length}</p>
       <div>
-        <button onClick={props.onRetry}>Повторить</button>
+        <Button type="primary" onClick={props.onRetry}>Повторить</Button>
+        <Button type="success" onClick={props.onRetry}>Перейти в список тестов</Button>
       </div>
     </div>
   );
