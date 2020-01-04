b6f50b2c5f63ae53873ca4d06d49f306be602ec3
PavelSolovev <pashius2010@gmail.com>
2020-01-05 01:08:22 +0400

    programm navigation

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -20,9 +20,7 @@ class App extends Component {
             </li>
             <li>
               <NavLink to={{
-                pathname: '/cars',
-                search: '?a=1&b=2',
-                hash: 'wfm-hash'
+                pathname: '/cars'
               }}>Cars</NavLink>
             </li>
           </ul>
