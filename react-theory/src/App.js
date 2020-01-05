745a4279e93be624f2b1ab1732c8bcd8fdf1a5fc
PavelSolovev <pashius2010@gmail.com>
2020-01-05 22:48:57 +0400

    routs protection

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -6,6 +6,10 @@ import Cars from "./Cars/Cars";
 import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
+  state = {
+    isLoggedIn: false
+  };
+
   render() {
     return (
       <div>
@@ -34,13 +38,26 @@ class App extends Component {
         </nav>
 
         <hr />
+
+        <div style={{ textAlign: "center" }}>
+          <h3>Is logged in {this.state.isLoggedIn ? "TRUE" : "FALSE"}</h3>
+          <button onClick={() => this.setState({ isLoggedIn: true })}>
+            Login
+          </button>
+        </div>
+
+        <hr />
         {/* localhost:3000 */}
         <Switch>
           <Route path="/" exact render={() => <h1>Home page</h1>} />
-          <Route path="/about" component={About} />
+
+          {this.state.isLoggedIn ? (
+            <Route path="/about" component={About} />
+          ) : null}
+
           <Route path="/cars/:name" component={CarDetail} />
           <Route path="/cars" exact component={Cars} />
-          <Redirect to ={'/'}/>
+          <Redirect to={"/"} />
           {/* <Route render={()=><h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>}/> */}
         </Switch>
       </div>
