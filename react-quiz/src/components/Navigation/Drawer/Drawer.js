acfb3a464f084f556c1a8b6ea2cc9c6bf55597d8
PavelSolovev <pashius2010@gmail.com>
2020-01-05 23:25:51 +0400

    Navigation routing

--- react-quiz/src/components/Navigation/Drawer/Drawer.js
+++ react-quiz/src/components/Navigation/Drawer/Drawer.js
@@ -1,42 +1,58 @@
-
-import React, { Component } from 'react';
-import classes from './Drawer.module.css';
-import Backdrop from '../../UI/Backdrop/Backdrop';
+import React, { Component } from "react";
+import classes from "./Drawer.module.css";
+import { NavLink } from "react-router-dom";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const links = [
-  1, 2, 3
+  {
+    to: "/",
+    label: "Список",
+    exact: true
+  },
+  {
+    to: "/auth",
+    label: "Авторизация",
+    exact: false
+  },
+  {
+    to: "/quiz-creator",
+    label: "Создать тест",
+    exact: false
+  }
 ];
 
 export class Drawer extends Component {
+  handleClick = () => {
+    this.props.onClose();
+  }
+
 
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <NavLink to={link.to} exact={link.exact} activeClassName={classes.active} onClick={this.handleClick}>
+            {link.label}
+          </NavLink>
         </li>
-      )
-    })
+      );
+    });
   }
-  
+
   render() {
-    const cls = [classes.Drawer]
-    if(!this.props.isOpen) {
-      cls.push(classes.close)
+    const cls = [classes.Drawer];
+    if (!this.props.isOpen) {
+      cls.push(classes.close);
     }
     return (
       <>
-      <nav className={cls.join(' ')}>
-        <ul>
-          { this.renderLinks()}
-
-        </ul>
-
-      </nav>
-      { this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null}
+        <nav className={cls.join(" ")}>
+          <ul>{this.renderLinks()}</ul>
+        </nav>
+        {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
       </>
-    )
+    );
   }
 }
 
-export default Drawer
+export default Drawer;
