b4d982a2a5c337ef29a1c6389833ac92a01e7886
PavelSolovev <pashius2010@gmail.com>
2020-01-09 00:30:06 +0400

    add input component

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
 
@@ -23,8 +24,15 @@ export default class Auth extends Component {
           <h1>Авторизация</h1>
 
           <form className={classes.AuthForm} onSubmit={this.submitHandler}>
-            <input type="text" />
-            <input type="text" />
+            <Input 
+              label="Email" 
+              type="text" 
+            />
+            <Input 
+              label="Пароль" 
+              type="text" 
+              errorMessage="TEST"
+            />
 
             <Button type="success" onClick={this.loginHandler}>
               Войти
