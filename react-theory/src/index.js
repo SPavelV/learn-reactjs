b954f6a8fd7867c6032b7e4a4daac772a6d603ef
Solovev Pavel <p.solovev@elewise.com>
2020-03-23 20:08:03 +0400

    add react-redux

--- react-theory/src/index.js
+++ react-theory/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/rootReducer';
+
+const store = createStore(rootReducer);
 
 const application = (
-  <BrowserRouter>
+  <Provider store={store}>
     <App/>
-  </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(application, document.getElementById('root'));
