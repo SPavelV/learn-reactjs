63ec37cbd4ee66e9b6433d4eb6667c778f2cc876
PavelSolovev <pashius2010@gmail.com>
2020-03-26 23:07:48 +0400

    async redux store

--- react-theory/src/index.js
+++ react-theory/src/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {createStore, applyMiddleware} from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './redux/rootReducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+import rootReducer from "./redux/rootReducer";
 
 const loggerMiddleware = store => next => action => {
   const result = next(action);
-  console.log('Middleware result: ', store.getState())
+  console.log("Middleware result: ", store.getState());
   return result;
-} 
+};
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(loggerMiddleware, reduxThunk)
+);
 
 const application = (
   <Provider store={store}>
-    <App/>
+    <App />
   </Provider>
-)
+);
 
-ReactDOM.render(application, document.getElementById('root'));
-
+ReactDOM.render(application, document.getElementById("root"));
