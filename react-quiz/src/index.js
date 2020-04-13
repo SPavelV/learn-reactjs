eb53b52cea39a75993e00506ba7d82952833c563
PavelSolovev <pashius2010@gmail.com>
2020-04-13 23:46:58 +0400

    add rudux

--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import * as firebase from "firebase/app";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer);
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
 
 const firebaseConfig = {
   apiKey: "AIzaSyDFRGLitIMIIuCmM4abOtTGVil7yCInU7I",
@@ -11,15 +23,17 @@ const firebaseConfig = {
   projectId: "quiz-react-ce508",
   storageBucket: "quiz-react-ce508.appspot.com",
   messagingSenderId: "1042884251547",
-  appId: "1:1042884251547:web:e14e1d661b4c3f49655b3e"
+  appId: "1:1042884251547:web:e14e1d661b4c3f49655b3e",
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
