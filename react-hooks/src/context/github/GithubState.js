3e30ff7f3c17a591ad8f6342132035782d58e112
Solovev Pavel <p.solovev@elewise.com>
2020-05-11 11:37:39 +0400

    github state part2

--- react-hooks/src/context/github/GithubState.js
+++ react-hooks/src/context/github/GithubState.js
@@ -1,21 +1,71 @@
 import React, { useReducer } from "react";
 import { GithubContext } from "./githubContext";
 import { githubReducer } from "./githubReducer";
+import {
+  SEARCH_USERS,
+  GET_USER,
+  GET_REPOS,
+  CLEAR_USERS,
+  SET_LOADING,
+} from "../type";
 
-export const GithubState = ({children}) => {
+export const GithubState = ({ children }) => {
   const initialState = {
     user: {},
     users: [],
     loading: false,
-    repos: []
-  }
+    repos: [],
+  };
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  return (
-    <GithubContext.Provider value={{
+  const search = async (value) => {
+    setLoading();
 
-    }}>
+    dispatch({
+      type: SEARCH_USERS,
+      payload: [],
+    });
+  };
+
+  const getUser = async (name) => {
+    setLoading();
+
+    dispatch({
+      type: GET_USER,
+      payload: {},
+    });
+  };
+
+  const getRepos = async (name) => {
+    setLoading();
+
+    dispatch({
+      type: GET_REPOS,
+      payload: [],
+    });
+  };
+
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
+
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
+  const { user, users, repos, loading } = state;
+
+  return (
+    <GithubContext.Provider
+      value={{
+        search,
+        getUser,
+        getRepos,
+        clearUsers,
+        setLoading,
+        user,
+        users,
+        repos,
+        loading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
-  )
-}
+  );
+};
