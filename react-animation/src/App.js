4caf725b355a06a70cccd6a5725eb9ffa2e0855f
PavelSolovev <pashius2010@gmail.com>
2020-05-16 01:15:51 +0400

    animation with transition

--- react-animation/src/App.js
+++ react-animation/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+
 import "./App.css";
+import { Transition } from "react-transition-group";
 
 function App() {
   const [toggle, setToggle] = useState(true);
@@ -10,7 +12,14 @@ function App() {
       <hr />
 
       <div className={"blocks"}>
-        {toggle && <div className="square blue">{toggle.toString()}</div>}
+        <Transition
+          in={toggle}
+          timeout={{ enter: 1000, exit: 500 }}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <div className={`square blue ${state}`}>{state}</div>}
+        </Transition>
       </div>
     </div>
   );
