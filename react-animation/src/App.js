e5b19a1acd434cc3bf15f08636e3627bb6f134c1
PavelSolovev <pashius2010@gmail.com>
2020-05-15 22:31:38 +0400

    add css animation

---
+++ react-animation/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <div className="App">
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      <hr />
+
+      <div className={"blocks"}>
+        {toggle && <div className="square blue">{toggle.toString()}</div>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
