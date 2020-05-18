570543fc65a87e8e6587b194702e09086f0d0b93
Solovev Pavel <p.solovev@elewise.com>
2020-05-18 19:06:34 +0400

    hoc app

--- react-theory/src/index.js
+++ react-theory/src/index.js
@@ -1,33 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import reduxThunk from "redux-thunk";
-import rootReducer from "./redux/rootReducer";
 
-const loggerMiddleware = store => next => action => {
-  const result = next(action);
-  console.log("Middleware result: ", store.getState());
-  return result;
-};
-
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
-    : compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk))
-);
-
-const application = (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-ReactDOM.render(application, document.getElementById("root"));
+ReactDOM.render( <App />, document.getElementById("root"));
